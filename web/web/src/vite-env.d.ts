3d383e8b3a5c8998408db21fdd40b7ac6b3aeb08
huzaifa <136922622+huzi0000@users.noreply.github.com>
2026-08-06 14:49:17 +0500

    Delete web/web directory

--- web/web/src/vite-env.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="vite/client" />
-
-interface Window {
-  ethereum?: {
-    request: (args: { method: string; params?: unknown[] }) => Promise<unknown>;
-    on?: (event: string, handler: (...args: unknown[]) => void) => void;
-    removeListener?: (event: string, handler: (...args: unknown[]) => void) => void;
-  };
-}
